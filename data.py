11d594caf7c1615309ac2158b50517b03fa42bef
Rodis-G <gustaf@rodebjer.se>
2025-12-26 23:04:36 +0100

    train/test

--- data.py
+++ data.py
@@ -761,6 +761,7 @@ ZIRAN (9041655)
 """
 import pandas as pd
 import re
+from sklearn.model_selection import train_test_split
 
 lines = raw_data.strip().split("\n")
 data_list = []
@@ -784,4 +785,9 @@ all_fleet_df = pd.read_csv("vessels.csv")
 all_fleet_df.set_index("IMO", inplace=True)
 all_fleet_df.drop("Name", axis=1, inplace=True)
 
-all_fleet_df["Shadow Fleet"] = all_fleet_df.index.isin(shadow_fleet_df.index)
+all_fleet_df["Shadow Fleet"] = all_fleet_df.index.isin(shadow_fleet_df.index.astype(int))
+
+all_fleet_df_DATA = all_fleet_df.drop('Shadow Fleet', axis=1)
+all_fleet_df_TARGET = all_fleet_df['Shadow Fleet']
+
+train_DATA, test_DATA, train_TARGET, test_TARGET = train_test_split(all_fleet_df_DATA, all_fleet_df_TARGET, test_size=0.2, random_state=42)
